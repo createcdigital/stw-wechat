f9d7c13b304f5a60a3bea763be0d9970d2fc910d
MansonOrYmc <543351696@qq.com>
2016-08-10 20:02:32 +0800

    update ticketinfo

--- resources/views/Journey/ticketinfo.blade.php
+++ resources/views/Journey/ticketinfo.blade.php
@@ -10,30 +10,29 @@
 
     <div class="ticket_content">
       <header>
-        <p><img src="{{asset('/image/24.jpg')}}"></p>
+        <p><img src="<?php if(!empty($ticketInfo[0])){echo $ticketInfo[0]->headimgurl;}?>"></p>
       </header>
       <div class="ticket_content_name"><?php if(!empty($ticketInfo[0])){echo $ticketInfo[0]->nickname;}?></div>
       <section>
         <ul class="ticket_section_ul">
           <li>
-            <p>套餐名称</p>
+            <p>套餐名称:</p>
             <div class="section_li_text"><?php if(!empty($ticketInfo[0])){echo $ticketInfo[0]->coupon_title;}?></div>
           </li>
           <li>
-            <p>套餐价格</p>
+            <p>套餐价格:</p>
             <span>¥<?php if(!empty($ticketInfo[0])){echo $ticketInfo[0]->coupon_set_price;}?></span>
           </li>
           <li>
-            <p>支付状态</p>
+            <p>支付状态:</p>
             <span><?php if(!empty($ticketInfo[0])){if($ticketInfo[0]->pay_status == 0){echo "未支付";}else{echo "已支付";}}?></span>
           </li>
           <li>
-            <p>购买时间</p>
+            <p>购买时间:</p>
             <span><?php if(!empty($ticketInfo[0])){echo $ticketInfo[0]->created_at;}?></span>
           </li>
         </ul>
       </section>
-      <footer>------支付信息-------</footer>
 
 
 
